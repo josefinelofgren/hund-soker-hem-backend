970af5f8bcbf5614df556998dc52bf552d87ad0a
Josefine Löfgren <josefinelofgren@Josefines-MacBook-Pro.local>
2022-03-16 15:04:55 +0100

    connect database

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-var express = require('express');
+const cors = require('cors');
+const express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const app = express();
+const port = process.env.PORT||4000;
+const host = '0.0.0.0';
+require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+// connect to database
+const MongoClient = require("mongodb").MongoClient;
+const { log } = require("console");
 
-app.use(logger('dev'));
+MongoClient.connect(process.env.DB_CLOUD, {
+  useUnifiedTopology: true,
+}).then((client) => {
+  console.log("Database connected");
+
+  const db = client.db("users");
+  app.locals.db = db;
+});
+
+app.use(cors());
+app.options('*', cors());
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/', (req, res) => {
+  console.log("Server responding")
+  res.send("Hello from Heroku");
+})
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+
+app.listen(port, host, () => {
+    console.log("Server running on port " + port);
+})
